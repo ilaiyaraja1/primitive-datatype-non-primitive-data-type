25c986df8df18ffa907f5cd6b8261e5ec4ced49c
ilaiyaraja1 <ilaiyaraja.bca@gmail.com>
2021-08-23 23:18:55 +0530

    datatype
    primitivedatatype & non-primitivedatatype.....

---
+++ src/One/two.java
@@ -0,0 +1,27 @@
+package One;
+ /*
+  * primitive datatype
+  * 
+  * datatype     defaultsize     value range
+  * ------------------------------------------
+  * 1.byte        1byte           -128 to 127  
+  * 2.int         4byte           -2,147,483,648 to 2,147,483,648
+  * 3.float       4byte            unlimited
+  * 4.double      8byte            unlimited
+  * 5.short       2byte           -32,768 to 32,768
+  * 6.long        8byte           -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+  * 8.char        2byte           Alpabets a,b,c,d,e,f.... we can use any one alpabets...
+  * 9.boolean     1bit            true & false  
+  * 
+  * 
+  * Non-primitive datatype
+  * 
+  * 1.classes
+  * 2.interface
+  * 3.Arrays
+  */
+public class two {
+	public static void main(String[] args) {
+
+	}
+}
